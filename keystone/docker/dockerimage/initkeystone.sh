1ccf741963f83df33fb22f7b5d61fcabf079b30f
Yongqiao Wang <grady.wang@foxmail.com>
2016-12-02 21:06:54 +0800

    Add init

---
+++ keystone/docker/dockerimage/initkeystone.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export OS_TOKEN=ADMIN_TOKEN
+export OS_URL=http://localhost:35357/v3
+export OS_IDENTITY_API_VERSION=3
+
+DEFAULT_ADMIN_USER=admin
+DEFAULT_ADMIN_PASS=Letmein123
+DEFAULT_ADMIN_ROLE=admin
+DEFAULT_DOMAIN=default
+
+EXTERNAL_IP=${1:-localhost}
+
+while true; do
+    </dev/tcp/127.0.0.1/35357 && break
+    sleep 3
+done
+
+
+openstack service show keystone || openstack service create --name keystone --description "OpenStack Identity" identity
+
+openstack endpoint list | awk '/keystone/ && /public/' | grep -w keystone || openstack endpoint create --region RegionOne identity public http://${EXTERNAL_IP}:5000/v3
+openstack endpoint list | awk '/keystone/ && /internal/' | grep -w keystone || openstack endpoint create --region RegionOne identity internal http://${EXTERNAL_IP}:5000/v3
+openstack endpoint list | awk '/keystone/ && /admin/' | grep -w keystone || openstack endpoint create --region RegionOne identity admin http://${EXTERNAL_IP}:35357/v3
+
+domain_id=$(openstack domain show ${DEFAULT_DOMAIN} -c id -f value || openstack domain create --description "Default Domain" ${DEFAULT_DOMAIN} -c id -f value)
+
+openstack user show ${DEFAULT_ADMIN_USER} || openstack user create --domain ${DEFAULT_DOMAIN} --password ${DEFAULT_ADMIN_PASS} ${DEFAULT_ADMIN_USER}
+
+role_id=$(openstack role show ${DEFAULT_ADMIN_ROLE} -c id -f value || openstack role create ${DEFAULT_ADMIN_ROLE} -c id -f value)
+openstack role assignment list | grep -w User || openstack role add --domain ${DEFAULT_DOMAIN} --user ${DEFAULT_ADMIN_USER} --user-domain ${DEFAULT_DOMAIN} ${DEFAULT_ADMIN_ROLE}
