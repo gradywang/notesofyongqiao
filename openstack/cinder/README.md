d6a8db16b4d1cb08d422594110ff89ce109ef899
Yongqiao Wang <grady.wang@foxmail.com>
2016-12-17 10:44:42 +0800

    add

--- openstack/cinder/README.md
+++ openstack/cinder/README.md
@@ -7,7 +7,7 @@ Before the following steps, it assumes that the keystone identity service has be
 # docker run --rm \
     --entrypoint=cp \
     -v "/openstack/scripts/cinder":/opt/cinder \
-    gradywang/cinder:mitaka /etc/cinder/init-cinder-db.sql /opt/cinder
+    gradywang/cinder:mitaka /etc/cinder/init-cinder-db.sh /opt/cinder
 
 # docker run --rm \
     --entrypoint=cp \
@@ -19,6 +19,7 @@ total 8
 -rw-r--r--. 1 root root  217 Dec 16 20:26 init-cinder-db.sql
 -rw-r--r--. 1 root root 2196 Dec 16 20:26 initkeystone.sh
 
+# docker exec mariadb /openstack/scripts/cinder/init-cinder-db.sh
 # docker exec keystone /openstack/scripts/cinder/initkeystone.sh
 ```
 
--- openstack/cinder/docker/dockerimage/cinder-api.sh
+++ openstack/cinder/docker/dockerimage/cinder-api.sh
@@ -6,6 +6,13 @@ DB_USER=${DB_USER:-cinder}
 DB_PASSWORD=${DB_PASSWORD:-Letmein123}
 DB_NAME=${DB_NAME:-cinder}
 
+RABBIT_USERID=${RABBIT_USERID:-openstack}
+RABBIT_PASSWORD=${RABBIT_PASSWORD:-Letmein123}
+RABBIT_HOST=${RABBIT_HOST:-127.0.0.1}
+RABBIT_PORT=${RABBIT_PORT:-5672}
+
+KEYSTONE_HOST=${KEYSTONE_HOST:-127.0.0.1}
+
 while true; do
     </dev/tcp/${MYSQL_SERVER_IP}/${MYSQL_SERVER_PORT} && break
     sleep 3
@@ -15,25 +22,31 @@ if [ -f /var/cinder/cinder.conf ]; then
     cp -f /var/cinder/cinder.conf /etc/cinder/cinder.conf
 fi
 
-if [ -f /var/cinder/policy.json ]; then
-    cp -f /var/cinder/policy.json /etc/cinder/policy.json
-fi
-
 sed -i "s/@@MYSQL_SERVER_IP@@/${MYSQL_SERVER_IP}/g" /etc/cinder/cinder.conf
 sed -i "s/@@MYSQL_SERVER_PORT@@/${MYSQL_SERVER_PORT}/g" /etc/cinder/cinder.conf
 sed -i "s/@@DB_USER@@/${DB_USER}/g" /etc/cinder/cinder.conf
 sed -i "s/@@DB_PASSWORD@@/${DB_PASSWORD}/g" /etc/cinder/cinder.conf
 sed -i "s/@@DB_NAME@@/${DB_NAME}/g" /etc/cinder/cinder.conf
 
-sed -i "s/@@MYSQL_SERVER_IP@@/${MYSQL_SERVER_IP}/g" /etc/cinder/initdb.sql
-sed -i "s/@@MYSQL_SERVER_PORT@@/${MYSQL_SERVER_PORT}/g" /etc/cinder/initdb.sql
-sed -i "s/@@DB_USER@@/${DB_USER}/g" /etc/cinder/initdb.sql
-sed -i "s/@@DB_PASSWORD@@/${DB_PASSWORD}/g" /etc/cinder/initdb.sql
-sed -i "s/@@DB_NAME@@/${DB_NAME}/g" /etc/cinder/initdb.sql
+sed -i "s/@@RABBIT_USERID@@/${RABBIT_USERID}/g" /etc/cinder/cinder.conf
+sed -i "s/@@RABBIT_PASSWORD@@/${RABBIT_PASSWORD}/g" /etc/cinder/cinder.conf
+sed -i "s/@@RABBIT_HOST@@/${RABBIT_HOST}/g" /etc/cinder/cinder.conf
+sed -i "s/@@RABBIT_PORT@@/${RABBIT_PORT}/g" /etc/cinder/cinder.conf
 
-mysql -h ${MYSQL_SERVER_IP} -p${MYSQL_SERVER_PASSWORD} < /etc/cinder/initdb.sql
+sed -i "s/@@KEYSTONE_HOST@@/${KEYSTONE_HOST}/g" /etc/cinder/cinder.conf
 
-keystone-manage db_sync
 
-keystone-all
+if [ -f /var/cinder/policy.json ]; then
+    cp -f /var/cinder/policy.json /etc/cinder/policy.json
+fi
+
+su -s /bin/sh -c "cinder-manage db sync" cinder
+
+service cinder-api restart
+
+while true
+do
+    sleep 10
+    echo "Cinder api is running ......"
+done
 
